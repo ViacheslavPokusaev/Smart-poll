f7482052a095f31ed6271df5f930a046e6c8c818
pokusaev-vyacheslav <vycheslavpokusaev@gmail.com>
2020-05-05 14:58:59 +0300

    start to develop asp.net core server

---
+++ ServerForFront/WebApplication1/Controllers/DefaultController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET: api/Default
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            User user1 = new User();
+            User user2 = new User();
+            user1.Name = "Viacheslav"; user2.Name = "Viacheslav1";
+            return new string[] { user1.Name, user2.Name };
+        }
+
+        // GET: api/Default/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Default
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Default/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
