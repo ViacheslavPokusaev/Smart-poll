53da4f8dbd707f875e4924c2189add8c89dcd1a1
pokusaev-vyacheslav <vycheslavpokusaev@gmail.com>
2020-05-06 15:10:18 +0300

    some problem with project

---
+++ ServerWebAPI/ServerWebAPI/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServerWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    internal class UserController : Controller
+    {
+        private IUserFunctional userFunctional;
+        public UserController(IUserFunctional userFunctional)
+        {
+            this.userFunctional = userFunctional;
+        }
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<List<User>> Get()
+        {
+            yield return userFunctional.GetUsers();
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ServerWebAPI/ServerWebAPI/Models/IUserFunctional.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServerWebAPI.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Email { get; set; }
+        public string UserPassword { get; set; }
+        public string UserName { get; set; }
+        public int Age { get; set; }
+    }
+    interface IUserFunctional
+    {
+        void Create(User user);
+        User Get(int IdUser);
+        List<User> GetUsers();
+    }
+
+    class UserFunctional : IUserFunctional
+    {
+        string connectionString = null;
+
+        public UserFunctional(string con)
+        {
+            this.connectionString = con;
+        }
+
+        public List<User> GetUsers()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users").ToList();
+            }
+        }
+
+        public User Get(int IdUser)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                return db.Query<User>("SELECT * FROM Users WHERE UserID = @UserID", new { IdUser }).FirstOrDefault();
+            }
+        }
+
+        public void Create(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age)";
+                db.Execute(sqlQuery, user);
+
+                // если мы хотим получить id добавленного пользователя
+                //var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
+                //int? userId = db.Query<int>(sqlQuery, user).FirstOrDefault();
+                //user.Id = userId.Value;
+            }
+        }
+    }
+}
