611e9905ce2ca8a9af85b2b7cb270e13ce0213d3
pokusaev-vyacheslav <vycheslavpokusaev@gmail.com>
2020-05-07 21:26:13 +0300

    some wrong actions

--- Front/usual.js
+++ Front/usual.js
@@ -11,3 +11,33 @@ var app2 = new Vue({
 		message: 'Вы загрузили эту страницу: ' + new Date().toLocaleString()
 	}
 })
+
+var app3 = new Vue({
+	el: '#app-3',
+	data: {
+		seen: true
+	}
+})
+
+var app4 = new Vue({
+	el: '#app-4',
+	data: {
+		todos: [
+			{ text: 'Изучить JavaScript' },
+			{ text: 'Изучить Vue' },
+			{ text: 'Создать что-нибудь классное' }
+		]
+	}
+})
+
+var app5 = new Vue({
+	el: '#app-5',
+	data: {
+		message: 'Привет, Vue.js!'
+	},
+	methods: {
+		reverseMessage: function () {
+			this.message = this.message.split('').reverse().join('')
+		}
+	}
+})
--- ServerCore/ServerCore/Models/Classes/UserAnswer.cs
+++ ServerCore/ServerCore/Models/Classes/UserAnswer.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace ServerCore.Models.Classes
 {
-    public class UserAnswer
-    {
-        public int IDAnswer { get; set; }
-        public int OptionID { get; set; }
-        public bool UserID { get; set; }
-    }
+	public class UserAnswer
+	{
+		public int IDAnswer { get; set; }
+		public int OptionID { get; set; }
+		public int UserID { get; set; }
+	}
 }
